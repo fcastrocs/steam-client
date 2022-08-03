62356830869ff9a748293de50c1f7f2968480b84
fcastrocs <44808362+fcastrocs@users.noreply.github.com>
2022-08-03 01:26:44 -0400

    describe tests

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -10,7 +10,7 @@ let steam: Steam = null;
 const accountName = "";
 const password = "";
 
-describe("steam-client", () => {
+describe("Test steam-client", () => {
   // connect to steam
   step("should connect to steam", async () => {
     const options: Options = { steamCM, timeout };
@@ -19,11 +19,11 @@ describe("steam-client", () => {
   });
 
   // test login
-  step("login", async () => {
+  step("login()", async () => {
     await steam.login({ accountName, password });
   });
 
-  it("cdkeyRedeem", async () => {
+  it("cdkeyRedeem()", async () => {
     await assert.rejects(steam.cdkeyRedeem("76CPE-E4CYG-5DXDG"), (err: Error) => {
       assert.equal(err.name, "steam-client");
       assert.equal(err.message, "DuplicateActivationCode");
@@ -31,21 +31,21 @@ describe("steam-client", () => {
     });
   });
 
-  it("getWebNonce - should return a string of length 19", async () => {
+  it("getWebNonce() - should return a string of length 19", async () => {
     const nonce = await steam.getWebNonce();
     assert.equal(nonce.length, 19);
   });
 
-  it("changeName", () => {
+  it("changeName()", () => {
     const name = "Someone" + Math.floor(Math.random() * 20);
     steam.changePlayerName(name);
   });
 
-  it("changePersonaState", () => steam.changePersonaState("snooze"));
+  it("changePersonaState()", () => steam.changePersonaState("snooze"));
 
-  it("idleGames", () => steam.idleGames([440]));
+  it("idleGames()", () => steam.idleGames([440]));
 
-  it("activateFreeToPlayGames", async () => {
+  it("activateFreeToPlayGames()", async () => {
     const games = await steam.activateFreeToPlayGames([570, 440, 730]);
 
     const gameIds = games.map((game) => {
@@ -56,14 +56,14 @@ describe("steam-client", () => {
     assert.equal(gameIds.includes(730), true);
   });
 
-  it("getPackagesInfo", async () => {
+  it("getPackagesInfo()", async () => {
     const res = await steam.getPackagesInfo([1]);
     assert.equal(res[0].appids.includes(10), true);
     assert.equal(res[0].appids.includes(20), true);
     assert.equal(res[0].appids.includes(30), true);
   });
 
-  it("getAppsInfo", async () => {
+  it("getAppsInfo()", async () => {
     const res = await steam.getAppsInfo([10]);
     assert.equal(res[0].name, "Counter-Strike");
   });
