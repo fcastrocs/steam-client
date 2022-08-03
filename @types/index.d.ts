1ed05f3da500d2ba279a2c66e32b6ce7c563232b
fcastrocs <44808362+fcastrocs@users.noreply.github.com>
2022-08-03 11:21:49 -0400

    better @types

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -4,32 +4,144 @@
 import Connection from "./connection";
 import { SocksClientOptions } from "socks";
 
-export interface Options {
-  steamCM: SocksClientOptions["destination"];
-  proxy?: SocksClientOptions["proxy"];
-  timeout?: number;
+export = Steam;
+
+declare namespace Steam {
+  interface Options {
+    steamCM: SocksClientOptions["destination"];
+    proxy?: SocksClientOptions["proxy"];
+    timeout?: number;
+  }
+
+  interface LoginOptions {
+    accountName: string;
+    password?: string;
+    machineName?: string;
+    clientOsType?: number;
+    shouldRememberPassword?: true;
+    twoFactorCode?: string;
+    loginKey?: string;
+    shaSentryfile?: Buffer;
+    authCode?: string;
+    protocolVersion?: 65580;
+    supportsRateLimitResponse?: true;
+    machineId?: Buffer;
+  }
+
+  type Sentry = Buffer;
+  type State = "offline" | "online" | "busy" | "away" | "snooze";
+
+  interface AccountAuth {
+    sentry: Buffer;
+    loginKey: string;
+    machineName: string;
+    webNonce: string;
+  }
+
+  interface AccountData {
+    steamId: string;
+    limited: boolean;
+    vac: boolean;
+    avatar: string;
+    nickname: string;
+    communityBanned: boolean;
+    locked: boolean;
+    games: AppInfo[];
+    emailOrDomain: string;
+    emailVerified: boolean;
+    secure: boolean;
+  }
+
+  interface SteamAccount {
+    auth: AccountAuth;
+    data: AccountData;
+  }
+
+  interface PackageInfo {
+    packageid: number;
+    billingtype: number;
+    licensetype: number;
+    status: number;
+    extended: [];
+    appids: number[];
+    depotids: number[];
+    appitems: [];
+  }
+
+  interface AppInfo {
+    logo: string;
+    logo_small: string;
+    name: string;
+    type: string;
+    gameid: number;
+  }
+
+  /**
+   * Internal interfaces
+   */
+  interface LooseObject {
+    [key: string]: any;
+  }
+
+  interface PersonaStateResponse {
+    personaState: number;
+    playerName: string;
+    avatarHash: Buffer;
+  }
+
+  interface ProductInfoResponse {
+    packages?: Package[];
+    apps?: App[];
+    metaDataOnly: boolean;
+    responsePending: boolean;
+  }
+
+  interface Package {
+    packageid: number;
+    changeNumber: number;
+    missingToken: boolean;
+    sha: Buffer;
+    buffer: Buffer;
+    size: number;
+  }
+
+  interface App {
+    appid: number;
+    changeNumber: number;
+    missingToken: boolean;
+    sha: Buffer;
+    buffer: Buffer;
+    onlyPublic;
+    size: number;
+  }
+
+  interface PackageBuffer {
+    [packageid: string]: PackageInfo;
+  }
+
+  interface AppBuffer {
+    appinfo: {
+      appid: number;
+      common: AppInfo;
+    };
+  }
+
+  interface ClientPurchaseResponse {
+    eresult: number;
+    purchaseResultDetails: number;
+    purchaseReceiptInfo: Buffer;
+  }
+
+  interface PurchaseReceiptInfo {
+    MessageObject: {
+      resultdetail: number; // same as purchaseResultDetails
+      packageid: number;
+      lineitems: { PackageID?: number; packageID?: number; packageid?: number }[];
+    };
+  }
 }
 
-export interface LoginOptions {
-  accountName: string;
-  password?: string;
-  machineName?: string;
-  clientOsType?: number;
-  shouldRememberPassword?: true;
-  twoFactorCode?: string;
-  loginKey?: string;
-  shaSentryfile?: Buffer;
-  authCode?: string;
-  protocolVersion?: 65580;
-  supportsRateLimitResponse?: true;
-  machineId?: Buffer;
-}
-
-export class SteamClientError extends Error {
-  constructor(message: string);
-}
-
-export default class Steam extends Connection {
+class Steam extends Connection {
   on(event: "disconnected", listener: (error: Error) => void): this;
   on(event: "loginKey", listener: (loginKey: string) => void): this;
   private personaState: number;
@@ -93,114 +205,6 @@ export default class Steam extends Connection {
   private createMachineName;
 }
 
-export type Sentry = Buffer;
-export type State = "offline" | "online" | "busy" | "away" | "snooze";
-
-export interface AccountAuth {
-  sentry: Buffer;
-  loginKey: string;
-  machineName: string;
-  webNonce: string;
-}
-
-export interface AccountData {
-  steamId: string;
-  limited: boolean;
-  vac: boolean;
-  avatar: string;
-  nickname: string;
-  communityBanned: boolean;
-  locked: boolean;
-  games: AppInfo[];
-  emailOrDomain: string;
-  emailVerified: boolean;
-  secure: boolean;
-}
-
-export interface SteamAccount {
-  auth: AccountAuth;
-  data: AccountData;
-}
-
-export interface PackageInfo {
-  packageid: number;
-  billingtype: number;
-  licensetype: number;
-  status: number;
-  extended: [];
-  appids: number[];
-  depotids: number[];
-  appitems: [];
-}
-
-export interface AppInfo {
-  logo: string;
-  logo_small: string;
-  name: string;
-  type: string;
-  gameid: number;
-}
-
-/**
- * Internal interfaces
- */
-interface LooseObject {
-  [key: string]: any;
-}
-
-interface PersonaStateResponse {
-  personaState: number;
-  playerName: string;
-  avatarHash: Buffer;
-}
-
-interface ProductInfoResponse {
-  packages?: Package[];
-  apps?: App[];
-  metaDataOnly: boolean;
-  responsePending: boolean;
-}
-
-interface Package {
-  packageid: number;
-  changeNumber: number;
-  missingToken: boolean;
-  sha: Buffer;
-  buffer: Buffer;
-  size: number;
-}
-
-interface App {
-  appid: number;
-  changeNumber: number;
-  missingToken: boolean;
-  sha: Buffer;
-  buffer: Buffer;
-  onlyPublic;
-  size: number;
-}
-
-interface PackageBuffer {
-  [packageid: string]: PackageInfo;
-}
-
-interface AppBuffer {
-  appinfo: {
-    appid: number;
-    common: AppInfo;
-  };
-}
-
-interface ClientPurchaseResponse {
-  eresult: number;
-  purchaseResultDetails: number;
-  purchaseReceiptInfo: Buffer;
-}
-
-interface PurchaseReceiptInfo {
-  MessageObject: {
-    resultdetail: number; // same as purchaseResultDetails
-    packageid: number;
-    lineitems: { PackageID?: number; packageID?: number; packageid?: number }[];
-  };
+export class SteamClientError extends Error {
+  constructor(message: string);
 }
