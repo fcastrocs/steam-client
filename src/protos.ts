1d06a5af7759b595a966a0849752b613e10d64c6
fcastrocs <44808362+fcastrocs@users.noreply.github.com>
2022-08-26 17:41:54 -0400

    proto encode/decode

--- src/protos.ts
+++ src/protos.ts
@@ -2,36 +2,19 @@
  * Proto encode and decoder
  */
 import { LooseObject } from "../@types";
-import { Type } from "protobufjs";
-import Steam from "./resources/index.js";
-const Protos = Steam.protos;
+import resources from "./resources/index.js";
+const Protos = resources.protos;
 
 export function decode(type: string, body: Buffer): LooseObject {
-  let proto: Type;
-  try {
-    proto = Protos.lookupType(type);
-  } catch (e) {
-    throw new Error(`Proto not found. ${type}`);
-  }
-
+  const proto = Protos.lookupType(type);
   const payload = proto.decode(body);
   return proto.toObject(payload);
 }
 
 export function encode(type: string, body: LooseObject) {
-  let proto: Type;
-  try {
-    proto = Protos.lookupType(type);
-  } catch (e) {
-    throw new Error(`Proto not found. ${type}`);
-  }
-
+  const proto = Protos.lookupType(type);
   const message = proto.create(body);
-
   const err = proto.verify(message);
-  if (err) {
-    throw new Error(err);
-  }
-
+  if (err) throw new Error(err);
   return Buffer.from(proto.encode(message).finish());
 }
