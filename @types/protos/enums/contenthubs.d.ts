6610f24bb5b8901b9615ab44e5de609a4f385cc0
Francisco Castro <fjcastrocs@gmail.com>
2023-11-05 23:44:10 -0500

    sync protos and enums

--- @types/protos/enums/contenthubs.d.ts
+++ @types/protos/enums/contenthubs.d.ts
@@ -1,6 +1,6 @@
 /**
  * Auto-generated file
- * Sun Nov 05 2023 20:01:03 GMT-0500 (Eastern Standard Time)
+ * Sun Nov 05 2023 23:43:25 GMT-0500 (Eastern Standard Time)
  */
 
 declare enum EContentHubDiscountFilterType {
